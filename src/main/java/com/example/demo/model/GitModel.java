a1bbab09b28b89fe923daee187def40ddcc3778a
chandu <chanduchowdary622@gmail.com>
2024-09-01 21:41:24 +0530

    getters and setters added

--- src/main/java/com/example/demo/model/GitModel.java
+++ src/main/java/com/example/demo/model/GitModel.java
@@ -2,4 +2,27 @@ package com.example.demo.model;
 
 public class GitModel {
 
+	private Integer gitId;
+	private String userName;
+	private String userAddress;
+	public Integer getGitId() {
+		return gitId;
+	}
+	public void setGitId(Integer gitId) {
+		this.gitId = gitId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserAddress() {
+		return userAddress;
+	}
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+	
+	
 }
